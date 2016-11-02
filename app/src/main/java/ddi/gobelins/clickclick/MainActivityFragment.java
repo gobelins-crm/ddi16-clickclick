ac1e8488559006168ea3f2182e3d545448b88c73
louisbl <louis@beltramo.me>
2016-11-02 07:24:15 +0100

    Bind to click on button in fragment

--- app/src/main/java/ddi/gobelins/clickclick/MainActivityFragment.java
+++ app/src/main/java/ddi/gobelins/clickclick/MainActivityFragment.java
@@ -5,11 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
 
     public MainActivityFragment() {
     }
@@ -17,6 +18,16 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // on click
     }
 }
